1be9e92c3be2388d6d70914051fc53a91243e0b5
masangonathi <nkosinathimasango@hotmail.com>
2025-01-28 21:40:18 +0200

    initial commit

---
+++ alx-rick-and-morty-app/interfaces/index.ts
@@ -0,0 +1,25 @@
+interface InfoProps {
+    pages: number
+    next: number
+    prev: number
+    count: number
+}
+
+export interface EpisodeProps {
+  id: number
+  name: string
+  air_date: string
+  episode: string
+  info: InfoProps
+}
+
+export type EpisodeCardProps = Pick<EpisodeProps, 'id' | 'name'| 'air_date' | "episode">
+
+
+export interface State {
+  hasError: boolean;
+}
+
+export interface ErrorBoundaryProps {
+  children: ReactNode;
+}
